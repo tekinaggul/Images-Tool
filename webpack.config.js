51fda204770018101a34c666274515adc67c39e9
Tekin AGGUL <tekinaggul@gmail.com>
2021-02-24 09:48:13 +0300

    Object class name has been changed

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
     path: path.join(__dirname, '/dist'),
     publicPath: '/',
     filename: 'bundle.js',
-    library: 'ImageTool',
+    library: 'ImagesTool',
     libraryTarget: 'umd',
     libraryExport: 'default'
   }
